55648f67b2af228d997cdfec88e1549f7170050a
mcm1957 <mcm57@gmx.at>
2023-01-17 19:09:57 +0100

    fix eslint errors

--- admin/words.js
+++ admin/words.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-unused-vars */
 /*global systemDictionary:true */
 'use strict';
 
 systemDictionary = {
-    'station_id': {
-        'en': 'station id',
-        'ru': 'station id',
-        'de': 'station id'
+    station_id: {
+        en: 'station id',
+        ru: 'station id',
+        de: 'station id',
     },
-    'morelogoutput': {
-        'en': 'more log output',
-        'ru': 'more log output',
-        'de': 'erweiterte log ausgaben'
-    }
+    morelogoutput: {
+        en: 'more log output',
+        ru: 'more log output',
+        de: 'erweiterte log ausgaben',
+    },
 };
